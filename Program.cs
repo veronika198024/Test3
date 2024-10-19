8af6f5b113cefe84782512c17bd6a7364afb1004
veronika198024 <veronika198024@gmail.com>
2024-10-19 18:50:45 +0300

    функция Аккермана

--- Program.cs
+++ Program.cs
@@ -1,15 +1,18 @@
-﻿//Вывод натуральных чисел в промежутке от M до N
-//static void Main(string[] args);
-int M = 1; // Можно поменять значение для проверки
-int N = 5; // Можно поменять значение для проверки
- static void PrintRange(int M, int N)
+﻿//Функция Аккермана
+int m = 2; // Можно поменять значение для проверки
+int n = 3; // Можно поменять значение для проверки
+int result = Ackermann(m, n);
+Console.WriteLine($"A({m}, {n}) = {result}");
+static int Ackermann(int m, int n)
 {
-if (M > N) return; // Базовый случай: если M больше N,прекращаем рекурсию
-Console.Write(M);
-if (M < N) Console.Write(", ");
-PrintRange(M + 1, N);
+    if (m == 0)
+        return n + 1;
+    else if (m > 0 && n == 0)
+        return Ackermann(m - 1, 1);
+    else if (m > 0 && n > 0)
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    return 0; // Для случая, если входные данные неверные
 }
 
-Console.WriteLine("Числа в промежутке:");
-PrintRange(M, N);
+
 
