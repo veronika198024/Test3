6c9414fb1d956323cdfa3594ff467540d22ed131
veronika198024 <veronika198024@gmail.com>
2024-10-19 18:42:38 +0300

    вывод натуральных чисел

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Вывод натуральных чисел в промежутке от M до N
+//static void Main(string[] args);
+int M = 1; // Можно поменять значение для проверки
+int N = 5; // Можно поменять значение для проверки
+ static void PrintRange(int M, int N)
+{
+if (M > N) return; // Базовый случай: если M больше N,прекращаем рекурсию
+Console.Write(M);
+if (M < N) Console.Write(", ");
+PrintRange(M + 1, N);
+}
+
+Console.WriteLine("Числа в промежутке:");
+PrintRange(M, N);
+
